9b0f301e96198d1618e2358fdae36d3210cf07d9
Esteban Santos <santosesteban09@gmail.com>
2021-10-07 21:23:21 -0500

    Initial stripe install

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,67 +1,7 @@
-<<<<<<< HEAD
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-
-
-
-
-
-checkout: async (parent, args, context) => {
-    const url = new URL(context.headers.referer).origin;
-    const order = new Order({ items: args.items });
-    const line_items = [];
-
-    const { items } = await order.populate('items').execPopulate();
-
-    for (let i = 0; i < items.length; i++) {
-      const item = await stripe.items.create({
-        name: items[i].name,
-        shortDescription: items[i].shortDescription,
-        images: [`${url}/images/${items[i].image}`]
-      });
-
-      const price = await stripe.prices.create({
-        item: item.id,
-        unit_amount: items[i].price * 100,
-        currency: 'usd',
-      });
-
-      line_items.push({
-        price: price.id,
-        quantity: 1
-      });
-    }
-
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items,
-      mode: 'payment',
-      success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${url}/`
-    });
-
-    return { session: session.id };
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
 const { AuthenticationError } = require("apollo-server-express");
 const { Items, User } = require("../models");
 const { signToken } = require("../utils/auth");
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const resolvers = {
   Query: {
@@ -72,6 +12,42 @@ const resolvers = {
 
       throw new AuthenticationError("You need to log in");
     },
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ items: args.items });
+      const line_items = [];
+  
+      const { items } = await order.populate('items').execPopulate();
+  
+      for (let i = 0; i < items.length; i++) {
+        const item = await stripe.items.create({
+          name: items[i].name,
+          shortDescription: items[i].shortDescription,
+          images: [`${url}/images/${items[i].image}`]
+        });
+  
+        const price = await stripe.prices.create({
+          item: item.id,
+          unit_amount: items[i].price * 100,
+          currency: 'usd',
+        });
+  
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+  
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+  
+      return { session: session.id };
+    }
   },
 
   Mutation: {
@@ -133,6 +109,5 @@ const resolvers = {
     },
   },
 };
->>>>>>> 8f8e0410be745ee1a189ec5634e012a7cab4b511
 
 module.exports = resolvers;

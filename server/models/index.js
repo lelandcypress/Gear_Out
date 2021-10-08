50df7aef3d23cba97333f09d48d996a02a1829e8
Justin Pickens <justin.pickens@gmx.com>
2021-10-07 20:19:16 -0500

    Thank me later Michael, I just saved us from disaster

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,15 @@
 const User = require("./User");
+const Items = require("./Items");
+const Order = require("./Order");
+const Vendor = require("./Vendor");
+const Rating = require("./Rating");
+const Image = require("./Image");
 
-module.exports = { User };
+module.exports = {
+    User,
+    Items,
+    Order,
+    Vendor,
+    Rating,
+    Image,
+};

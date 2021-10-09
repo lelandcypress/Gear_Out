28a664ec763c5204b5642c2bb965e736bae583d3
Michael Souter <souterm1@gmail.com>
2021-10-09 12:55:13 -0400

    backend patches

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,15 @@
 const User = require("./User");
-const Items = require("./Items");
-const Order = require("./Order");
+const { Items } = require("./Items");
+const { Order } = require("./Order");
 const Vendor = require("./Vendor");
-const Rating = require("./Rating");
+const { Rating } = require("./Rating");
 const Image = require("./Image");
 
 module.exports = {
-    User,
-    Items,
-    Order,
-    Vendor,
-    Rating,
-    Image,
+  User,
+  Items,
+  Order,
+  Vendor,
+  Rating,
+  Image,
 };

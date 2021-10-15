55d1990e76c41414330f07a39585d2cedf331543
Michael Souter <souterm1@gmail.com>
2021-10-15 12:42:26 -0400

    Readme Finished

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -14,6 +14,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import "./User.css";
+import Container from "react-bootstrap/Container";
 
 const UserProfile = (props) => {
   const [returnItem, { error }] = useMutation(MUTATION_RETURN_ITEM);
@@ -41,40 +42,6 @@ const UserProfile = (props) => {
   };
 
   return (
-<<<<<<< HEAD
-    <Row>
-      <Col>
-        <Card className="m-3 profile-shadow">
-          <Card.Header>
-            <h3>Profile</h3>
-          </Card.Header>
-          <Card.Body>
-            <Card.Text>Username: {user.username}</Card.Text>
-            <Card.Text>Email: {user.email}</Card.Text>
-          </Card.Body>
-          <div className="border custom-stack">
-            <p>User Reviews</p>
-            <div>5 out of 5 Stars</div>
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
-          </div>
-        </Card>
-      </Col>
-      <Col>
-        <Card className="m-3 profile-shadow">
-          <Card.Header>
-            <h3>Orders</h3>{" "}
-          </Card.Header>
-          {user.orders ? (
-            <>
-              {user.orders.map((order) => {
-                return (
-                  <Card.Body>
-                    <p>Rental Start:{order.startDate}</p>
-                    <p>Due Back:{order.endDate}</p>
-=======
     <Container>
       <Row className="m-2">
         <Col>
@@ -107,26 +74,23 @@ const UserProfile = (props) => {
                 {user.orders.map((order) => {
                   return (
                     <Card.Body>
-                      <Card.Text border="dark">
-                        <p>Rental Start:{order.startDate}</p>
-                        <p>Due Back:{order.endDate}</p>
->>>>>>> master
+                      <p>Rental Start:{order.startDate}</p>
+                      <p>Due Back:{order.endDate}</p>
 
-                    {order.items.map((item) => (
-                      <p>{item.name}</p>
-                    ))}
+                      {order.items.map((item) => (
+                        <p>{item.name}</p>
+                      ))}
 
-                    <Button onClick={handleReturn}>Return</Button>
-                  </Card.Body>
-                );
-              })}
-            </>
-          ) : null}
-
-          <Button onClick={handleReturn}>Return</Button>
-        </Card>
-      </Col>
-    </Row>
+                      <Button onClick={handleReturn}>Return</Button>
+                    </Card.Body>
+                  );
+                })}
+              </>
+            ) : null}
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
